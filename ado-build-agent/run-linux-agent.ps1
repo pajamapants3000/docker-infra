042a6548893aff7ba04b97d029f701eb61a2e876
Tommy Lincoln <pajamapants3000@gmail.com>
2020-11-24 03:56:12 -0500

    Remove sensitive info and make PAT a script argument instead

--- ado-build-agent/run-linux-agent.ps1
+++ ado-build-agent/run-linux-agent.ps1
@@ -1,9 +1,11 @@
+param ($pat)
+
 $AGENT_VOLUME_NAME="ado_build_agent"
 $AGENT_VOLUME_DIR="/ado_build_agent"
 
 docker run -d `
     -e AZP_URL="https://dev.azure.com/pajamapants3000" `
-    -e AZP_TOKEN="mgscfvqtaf2o536us5uw7brifw2oxtxx6ezrnc3kqvemrxpyq2tq" `
+    -e AZP_TOKEN="$pat" `
     -e AZP_AGENT_NAME=docker-linux-1 `
     -e CONTAINER_REGISTRY="my-registry:55000" `
     -e AGENT_VOLUME_NAME=${AGENT_VOLUME_NAME} `
